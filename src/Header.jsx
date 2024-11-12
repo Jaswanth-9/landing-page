9805705c8a2fbd77011bc82f8958328c56ee6077
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-12 11:23:51 +0530

    header created

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,16 @@
+import "./Header.css"
+
 export default function Header(){
     return(
-        <div>
-            
-        </div>
+        <main className = "Header">
+            <img src="/images/puma logo.png" alt="brand logo"/>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>  
+            <button>Login</button>
+        </main>
     )
 }
